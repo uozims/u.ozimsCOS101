a4ff583839041d2708d39a88bdeae4f6c92282cb
uozims <urechi.ozims@pau.edu.ng>
2026-09-02 15:17:41 +0100

    class pratice

---
+++ week-2/practice_2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!(); // prints just a ...
+	println!("hello "); // prints hello
+	println!("format {} arguments", "some"); //prints format some arguments
+	println!("My name is {}. I am the class rep of stream 1", "Urechi Victoria Ozims");	
+
+}
