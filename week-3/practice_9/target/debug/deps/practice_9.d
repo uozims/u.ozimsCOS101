9730d4f24a0c14af27a7e8d1abb334c7d656e3b1
uozims <urechi.ozims@pau.edu.ng>
2026-09-10 16:19:07 +0100

    first commit

---
+++ week-3/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\urebu\OneDrive\Documents\u.ozimsCOS101\week-3\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\urebu\OneDrive\Documents\u.ozimsCOS101\week-3\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\urebu\OneDrive\Documents\u.ozimsCOS101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\urebu\OneDrive\Documents\u.ozimsCOS101\week-3\practice_9\src\main.rs
