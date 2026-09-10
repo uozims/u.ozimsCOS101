f465dba4ba3c12c7c9a28ab82ebed101017e79d4
uozims <urechi.ozims@pau.edu.ng>
2026-09-10 15:58:44 +0100

    first commit

---
+++ week-3/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\urebu\OneDrive\Documents\u.ozimsCOS101\week-3\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\urebu\OneDrive\Documents\u.ozimsCOS101\week-3\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
